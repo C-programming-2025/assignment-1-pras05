86bb1a886d586c375c01a34e6ce2fb20016cad28
Pras05 <prashnarai05@gmail.com>
2025-08-28 23:05:28 +0545

    Prashna

--- src/q20.c
+++ src/q20.c
@@ -2,8 +2,13 @@
 
 int main() {
     int n;
+    int pras;
+
     printf("Enter a number: ");
     scanf("%d", &n);
-    printf("%d\n", (n % 3 == 0) || (n % 5 == 0));
+
+    pras= (n % 3 == 0) || (n % 5 == 0);
+
+    printf("%d\n", pras);
     return 0;
 }
